ad4c46ae5bbe75a3dd91e3cec741fec7be22b22f
Rudo Togara <r.togara@alustudent.com>
2021-06-27 16:30:18 +0200

    added the author page

---
+++ html/author.js
@@ -0,0 +1,25 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  function openForm1() {
+    document.getElementById("myForm2").style.display = "block";
+  }
+  function openForm2() {
+    document.getElementById("myForm3").style.display = "block";
+  }
+  function openForm3() {
+    document.getElementById("myForm4").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  function closeForm1() {
+    document.getElementById("myForm2").style.display = "none";
+  }
+  function closeForm2() {
+    document.getElementById("myForm3").style.display = "none";
+  }
+  function closeForm3() {
+    document.getElementById("myForm4").style.display = "none";
+  }
