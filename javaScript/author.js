305e403cd229905ced33bfcd160325ae9e7643d1
Cynthia Iradukunda <cynthia@Cynthias-MacBook-Pro.local>
2021-07-21 19:01:10 +0200

    added the css

--- javaScript/author.js
+++ javaScript/author.js
@@ -45,29 +45,41 @@ const profiles = [
 
  let leftElement = 0; 
  let rightElement = 1; 
+ let element = 0; 
 const leftSide = () => {
-      if(leftElement > 0){
+      if(leftElement > 0 &&  window.innerWidth > 1024 ){
         let removeDiv = document.getElementById(rightElement);
         let currentDiv = document.getElementById(leftElement);
         let addDiv =  createElement(--leftElement);
          document.getElementById("profiles").insertBefore(addDiv, currentDiv);
          document.getElementById("profiles").removeChild(removeDiv);
          rightElement--;
-}
+     }else if(element >0 && window.innerWidth < 1023){
+      let removeDiv = document.getElementById(element);
+      let addDiv =  createElement(--element);
+       document.getElementById("profiles").appendChild(addDiv);
+      document.getElementById("profiles").removeChild(removeDiv);
+     }
 }
 const rightSide = () => {
-  if(rightElement <profiles.length-1){
+  if(rightElement <profiles.length-1 && window.innerWidth > 1024){
    let removeDiv = document.getElementById(leftElement);
     let addDiv =  createElement(++rightElement);
     document.getElementById("profiles").appendChild(addDiv);
     document.getElementById("profiles").removeChild(removeDiv);
    leftElement++;
+ }else if(element <profiles.length-1 && window.innerWidth < 1023){
+   let removeDiv =  document.getElementById(element);
+   let addDiv = createElement(++element);
+   document.getElementById("profiles").appendChild(addDiv);
+   document.getElementById("profiles").removeChild(removeDiv);
  }
 }
 
  const addProfiles = () =>{
      let i= 0; 
-     while(i < 2){
+      let max  = (window.innerWidth >1023)? 2 : 1; 
+     while(i < max){
        let div = createElement(i);
            document.getElementById("profiles").appendChild(div);
            i++; 
