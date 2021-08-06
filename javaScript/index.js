23e38b36a2a41b74698f5d746db9617c3af762a9
reynaldjoabet <reynaldjoabet@gmail.com>
2021-08-06 07:47:03 +0100

    few changes and fixes.Completed page

---
+++ javaScript/index.js
@@ -0,0 +1,64 @@
+   
+
+let loading = true; 
+if(loading)document.getElementById("loader").style.display= "flex";
+
+const fetchData = (async () => {
+      await fetch("https://api.reliefweb.int/v1/disasters?appname=rwint-user-0&profile=list&preset=latest&slim=1&query[value]=%22earthquake%22%20OR%20%22flood%22%20OR%20%22Tropical%20Cylone%22&query[operator]=AND")
+        .then((response) =>{
+           return response.json()
+        })
+        .then((data) => {
+            let count = 0; 
+            data.data.forEach(disaster =>{
+                if(count<2){
+                    let types = disaster.fields.type.map(value =>{
+                        return value.name;
+                   });
+                   let countries = disaster.fields.country.map(value =>{
+                    return value.name;
+                   }).join(",");
+
+                    let div = document.createElement("div"); 
+                    let div1 = document.createElement("div");
+                    let div2 = document.createElement("div");
+                    div.setAttribute("class", "news")
+                    div1.setAttribute("class", "div1")
+                    div2.setAttribute("class", "div2")
+                    let img = document.createElement("img");
+                     img.src= (types[0] == "Flood" || "Flash Flood")? "./images/overview/Flood.png": (types[0]== "Earthquake")? "./images/overview/earthquakes.png":"./images/Cyclone.png"
+                    div1.appendChild(img);
+                    let status = capitalizeFirstLetter(disaster.fields.status);
+                    let h3 = document.createElement('h3');
+                    let p = document.createElement('p');
+                    let date = new Date(disaster.fields.date.created);
+                    h3.innerHTML = `${disaster.fields.name}`
+                    p.innerHTML = `<span><i class="fa fa-flag"></i> Countries Affected: ${countries}</span></br>
+                                    <span class=${disaster.fields.status} == "current"? "current":${disaster.fields.status} == "alert"? "alert": "past">Status: ${status}</span></br>
+                                    <span><i class="fa fa-bolt"></i> Disaster Type: ${types}</span></br>
+                                    <span><i class="fa fa-clock-o"></i> Time: ${date}</span></br>
+                                    <div><a href="${disaster.fields.url}" target="_blank">Read More</a></div>`
+                    div2.appendChild(h3);
+                    div2.appendChild(p);
+                    div.appendChild(div1);
+                    div.appendChild(div2);
+                
+                     document.getElementById("latestNews").appendChild(div)
+                }
+              count++; 
+            })
+        })
+        .catch((error)=> {
+            console.log(error)
+        })
+        loading = false;
+        if(loading == false) document.getElementById("loader").style.display= "none";
+});
+
+setTimeout(() => {
+    fetchData()
+}, 3000);
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
